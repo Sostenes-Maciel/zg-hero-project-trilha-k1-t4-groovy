d1a01ee71f11fe6c7f55202c1d980d0843b9b3c9
Sostenes-Maciel <sosthemaciel@hotmail.com>
2026-09-05 14:59:00 -0300

    feat: cria entidades Candidato e Empresa herdando de Pessoa e IPessoa - v0.3.0

---
+++ src/main/groovy/br/com/zg/acelera/model/IPessoa.groovy
@@ -0,0 +1,15 @@
+package br.com.zg.acelera.model
+
+interface IPessoa {
+
+    String getNome()
+    String getEmail()
+    String getEstado()
+    String getCep()
+    String getDescricao()
+
+    List<String> getCompetencias()
+
+
+
+}
--- src/main/groovy/br/com/zg/acelera/model/Pessoa.groovy
+++
@@ -1,7 +0,0 @@
-package br.com.zg.acelera.model
-
-interface Pessoa {
-
-
-
-}
