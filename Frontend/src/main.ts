63a570db6951a83f72a70f2f23dd6cbf3d560f0d
Sostenes-Maciel <sosthemaciel@hotmail.com>
2026-09-20 18:27:50 -0300

    style(frontend): cria base visual - v0.31.4

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,9 +3,7 @@ import {BancodeDados} from './repository/BancodeDados'
 import { configurarMenu } from './components/menu'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = '' +
-    '<h1>LinkerTinder</h1>'
-'<p>Frontend em TypeScript</p>'
-
+    '<h1>Seja Bem-vindo(a) ao LinkerTinder</h1>'
 
 BancodeDados.inicializar()
 
